5127787b082006cbaa1e5ae7ad59ff3a525087af
vinodbistvb <vinodbistvb@gmail.com>
2021-08-21 14:36:19 +0530

    first step

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#reserveTableButton').click(function(){
+        $('#reservationModal').modal('toggle');
+    });
+    $('#loginButton').click(function(){
+        $('#loginModal').modal('toggle');
+    });
+    $('#mycarousel').carousel({interval:2000});
+    $('#carouselButton').click(function(){
+        if ($('#carouselButton').children('span').hasClass('fa-pause')){
+                $('#mycarousel').carousel('pause');
+                $('#carouselButton').children('span').removeClass('fa-pause');
+                $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')){
+                $('#mycarousel').carousel('cycle');
+                $('#carouselButton').children('span').removeClass('fa-play');
+                $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+})
